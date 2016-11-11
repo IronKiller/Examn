d09f7cf6116b65bc4247bc61212029b72162f57e
yukitogithub <lujanfernandogabriel@gmail.com>
2016-11-11 18:49:51 -0300

    El examen feito todo incompleto

---
+++ PetStore/PetStore/Models/Pet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetStore.Models
+{
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public State State { get; set; }
+        public Animal AnimalType { get; set; }
+
+    }
+
+    public enum State
+    {
+        FreeForAdoption,
+        FreeForSale
+    }
+
+    public enum Animal
+    {
+        Dog,
+        Cat,
+        Hamster,
+        Rabbit
+    }
+
+}
